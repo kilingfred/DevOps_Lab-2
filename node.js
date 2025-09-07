23dd8cccad39e0b0a175aaface422a49c796f06e
kilingfred <mr.perepolkin@gmail.com>
2025-09-07 16:38:14 +0300

    Fifth of many

--- node.js
+++ node.js
@@ -1,19 +1,22 @@
 const http = require("http");
 const fs = require("fs");
 const path = require("path");
+const PORT = 3000;
+const HOSTNAME = "localhost";
 
-const server = http
+http
   .createServer((req, res) => {
-    const filePath = path.join(
-      __dirname,
-      "public",
-      req.url === "/" ? "index.html" : req.url
-    );
-    const extname = path.extname(filePath);
-    let contentType = "text/html";
+    const filePath = path.join(__dirname, "index.html");
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(500, { "Content-Type": "text/plain" });
+        res.end("Internal Server Error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
   })
   .listen(PORT, HOSTNAME, () => {
     console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
   });
-const PORT = 3000;
-const HOSTNAME = "localhost";
